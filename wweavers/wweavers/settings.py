ca85029b9fdcb8a0aa875ad65e1d2c713b929b01
t_barun <pujanthapa88.63@gmail.com>
2019-07-30 14:28:18 +0545

    Static and media url settings added

--- wweavers/wweavers/settings.py
+++ wweavers/wweavers/settings.py
@@ -120,9 +120,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_ROOT = "/home/anupam/staticfiles"
 STATIC_URL = '/static/'
-# STATICFILES_DIRS = [
-#         os.path.join(BASE_DIR, 'static'),
-#     ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+FILE_UPLOAD_DIRECTORY_PERMISSIONS = 0o0777
+FILE_UPLOAD_PERMISSIONS = 0o0644
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'staticfiles')
+]
